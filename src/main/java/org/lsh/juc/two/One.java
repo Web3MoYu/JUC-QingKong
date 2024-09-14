2aee0c05c83091281968ac795d597f011a6014d5
lsh <3040114965@qq.com>
2024-09-14 20:55:58 +0800

    AtomicInteger

---
+++ src/main/java/org/lsh/juc/two/One.java
@@ -0,0 +1,22 @@
+package org.lsh.juc.two;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class One {
+
+    // 原子类，能够保证原子性
+    private static AtomicInteger i = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable r = () -> {
+            for (int j = 0; j < 1000; j++) {
+                i.getAndIncrement();
+            }
+        };
+        new Thread(r).start();
+        new Thread(r).start();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(i.get());
+    }
+}
